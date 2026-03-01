45abfac4e01533e8f7e461de73c93494e1287465
Saryn Mooney <saryn@activatecare.com>
2026-03-01 16:09:58 -0700

    create asset spec

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Asset, type: :model do
       expect(asset).to be_valid
     end
   end
+
+  # describe 'associations' do
+  #   it 'belongs to a company' do
+  #     asset = create(:asset)
+  #     expect(asset.company).to be_present
+  #   end
+  # end
 end
